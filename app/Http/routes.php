325a6d1a375bb66317a6d21998df1ccbebf3d67c
hengshuangliu <767924520@qq.com>
2017-06-02 11:22:26 +0800

    add passwordreset route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,14 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
+// 密码重置链接的路由...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// 密码重置的路由...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
 // Restaurant路由
 Route::get('/restaurants', 'RestaurantController@index');
 Route::post('/restaurant', 'RestaurantController@store');
