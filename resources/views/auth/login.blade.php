b0db68cbd20c55f880ffce242c2c8c1bb7f32a33
unknown <>
2017-06-04 18:30:00 +0800

    edit login.pic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,28 +5,9 @@
     <div class="container">
         <div class="row clearfix">
             <div class="col-md-12 column">
-                <div class="carousel slide" id="carousel-469922">
-                <ol class="carousel-indicators">
-                    <li class="active" data-slide-to="0" data-target="#carousel-469922">
-                    </li>
-                    <li data-slide-to="1" data-target="#carousel-469922">
-                    </li>
-                    <li data-slide-to="2" data-target="#carousel-469922">
-                    </li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img alt="" src="http://ibootstrap-file.b0.upaiyun.com/lorempixel.com/1600/500/sports/1/default.jpg"
-                        />
-                    </div>
-                    <div class="item">
-                        <img alt="" src="http://ibootstrap-file.b0.upaiyun.com/lorempixel.com/1600/500/sports/2/default.jpg" />
-                    </div>
-                    <div class="item">
-                        <img alt="" src="http://ibootstrap-file.b0.upaiyun.com/lorempixel.com/1600/500/sports/3/default.jpg" />
-                    </div>
-                </div> <a class="left carousel-control" href="#carousel-469922" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-469922" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
+                <div class="promotion-banner">
+                     <img src="//s0.meituan.net/bs/file/?f&#x3D;fe-sso-fs:build/page/static/banner/www.jpg" width=100% height="40%"/>
+                </div>
         </div>
 
             <!-- Display Validation Errors -->
