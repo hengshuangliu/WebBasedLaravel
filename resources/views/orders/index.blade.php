27e9ed82a2fd1dd9ca8d2168dd556fc5e30374e1
unknown <>
2017-06-05 17:16:59 +0800

    list

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -14,6 +14,33 @@
                     <!-- Display Validation Errors -->
                     @include('common.errors')
                         <div class="panel-body">
+                            <table  style="float:right；margin-right:0px" class="table table-striped">
+                                <thead>
+                                    <th>已完成订单</th>
+                                    <th>&nbsp;</th>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th width="20%" class="table-text"><div>桌号</div></th>
+                                    <th width="20%" class="table-text"><div>金额</div></th>
+                                    <th width="20%" class="table-text"><div>时间</div></th>
+                                    <th width="20%" class="table-text"><div>备注</div></th>
+                                    <th width="20%" class="table-text"><div> </div></th>
+                                </tr>
+                                @foreach ($orders as $order)
+                                @if($order->status === "Finished")
+                                <tr>
+                                    <td class="table-text"><div>{{ $order->alias }}</div></td>
+                                    <td class="table-text"><div>{{ $order->total }}元</div></td>
+                                    <td class="table-text"><div>{{ $order->updated_at }}</div></td>
+                                    <td class="table-text"><div>{{ $order->remark }}</div></td>
+                                    <td class="table-text"><div> </div></td>
+                                </tr>
+                                @endif
+                                @endforeach
+
+                                </tbody>
+                            </table>
                             <table class="table table-striped task-table">
                                 <thead>
                                     <th>未完成订单</th>
@@ -21,11 +48,11 @@
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td class="table-text"><div>桌号</div></td>
-                                    <td class="table-text"><div>金额</div></td>
-                                    <td class="table-text"><div>时间</div></td>
-                                    <td class="table-text"><div>备注</div></td>
-                                    <td class="table-text"><div>当前状态</div></td>
+                                    <th width="20%" class="table-text"><div>桌号</div></th>
+                                    <th width="20%" class="table-text"><div>金额</div></th>
+                                    <th width="20%" class="table-text"><div>时间</div></th>
+                                    <th width="20%" class="table-text"><div>备注</div></th>
+                                    <th width="20%" class="table-text"><div> </div></th>
                                 </tr>
                                 @foreach ($orders as $order)
                                 @if($order->status === "Enjoying")
@@ -35,11 +62,11 @@
                                     <td class="table-text"><div>{{ $order->updated_at }}</div></td>
                                     <td class="table-text"><div>{{ $order->remark }}</div></td>
                                     <!-- Order modify Button -->
-                                    <td>
+                                    <td class="table-text">
                                         <form action="/order/modify/{{$order->id}}" method="POST">
                                         {{ csrf_field() }}
                                         <button type="submit" id="delete-dish-{{ $order->id }}" class="btn btn-default">
-                                            <i></i>已结账
+                                            <i></i>结账
                                         </button>
                                         </form>
                                     </td>
@@ -49,39 +76,7 @@
 
                                 </tbody>
                             </table>
-                            <table class="table table-striped task-table">
-                                <thead>
-                                    <th>已完成订单</th>
-                                    <th>&nbsp;</th>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td class="table-text"><div>桌号</div></td>
-                                    <td class="table-text"><div>金额</div></td>
-                                    <td class="table-text"><div>时间</div></td>
-                                    <td class="table-text"><div>备注</div></td>
-                                    <td class="table-text"><div>当前状态</div></td>
-                                </tr>
-                                @foreach ($orders as $order)
-                                @if($order->status === "Finished")
-                                <tr>
-                                    <td class="table-text"><div>{{ $order->alias }}</div></td>
-                                    <td class="table-text"><div>{{ $order->total }}元</div></td>
-                                    <td class="table-text"><div>{{ $order->updated_at }}</div></td>
-                                    <td class="table-text"><div>{{ $order->remark }}</div></td>
-                                     <td>
-                                        <form >
-                                        <button class="btn btn-default">
-                                            <i></i>已完成
-                                        </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endif
-                                @endforeach
 
-                                </tbody>
-                            </table>
 
                             <table class="table table-striped task-table">
                                 <thead>
@@ -90,11 +85,11 @@
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td class="table-text"><div>桌号</div></td>
-                                    <td class="table-text"><div>金额</div></td>
-                                    <td class="table-text"><div>时间</div></td>
-                                    <td class="table-text"><div>备注</div></td>
-                                    <td class="table-text"><div>当前状态</div></td>
+                                    <th width="20%" class="table-text"><div>桌号</div></th>
+                                    <th width="20%" class="table-text"><div>金额</div></th>
+                                    <th width="20%" class="table-text"><div>时间</div></th>
+                                    <th width="20%" class="table-text"><div>备注</div></th>
+                                    <th width="20%" class="table-text"><div> </div></th>
                                 </tr>
                                 @foreach ($orders as $order)
                                 @if($order->status === "NotConfirm")
@@ -105,11 +100,11 @@
                                     <td class="table-text"><div>{{ $order->updated_at }}</div></td>
                                     <td class="table-text"><div>{{ $order->remark }}</div></td>
                                     <!-- Order modify Button -->
-                                    <td>
+                                    <td class="table-text">
                                         <form action="/order/modify/{{$order->id}}" method="POST">
                                         {{ csrf_field() }}
                                         <button type="submit" id="delete-dish-{{ $order->id }}" class="btn btn-default">
-                                            <i></i>已确认
+                                            <i></i>删除
                                         </button>
                                         </form>
                                     </td>
