3d209478822a6e9833d59835bcf5174c3cf4db87
unknown <>
2017-06-12 12:23:12 +0800

    dish-list

--- resources/views/guest/indexnew.blade.php
+++ resources/views/guest/indexnew.blade.php
@@ -6,9 +6,9 @@
                     'dishesRestaurant' => $dishesRestaurant, -->
 @section('content')
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div>
             @if (($order->status) == "NotConfirm")
-            <div class="col-md-12 column">
+            <div class="col-xs-12 column">
             <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
             <div>
             <ul class="nav navbar-nav nav-tabs">
@@ -18,53 +18,46 @@
             </div>
             </nav>
             </div>
+        </div>
 
-            <div class="col-md-12 column">
-            <a name="menu"></a>
-            <div class="panel panel-default bg-info">
-            <div class="panel-heading">
-                <div class="text-danger">
-                <h4>
+            <div>
+                <legend  style="font-size: 200%">
                     {{$restaurant->name}} <small>桌名：{{$table->alias}}</small>
-                </h4>
-                </div>
-
-                </div>
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
+                </legend>
+                 <!-- Display Validation Errors -->
                     @include('common.errors')
-                        <div name="menu" class="panel-body">
-                        @foreach ($dishesRestaurant as $dish)
-                        <div class="col-sm-6 col-md-3"  style="padding: 20px 20px 20px 20px;">
-                              <div>
-                                 <img src={{asset('uploads/'.($dish->id).'.jpg')}} alt="图片加载失败" width="150" height="150" class="thumbnail" >
-                              </div>
-                              <div class="caption">
-                                 <h5>{{ $dish->name }}</h5> <h4>售价：{{ $dish->price }}</h4>
-                                 <p>{{ $dish->description }}</p>
-                                 <p>
-                                    <button onclick="ajaxLoad('/guest/create/{{$dish->id}}/{{$order ->id}}')"  class="btn"><i class=""></i>Add</button>
-                                 </p>
-                              </div>
-                           </div>
-                        @endforeach
-                        </div>
-                </div>
+                    @foreach ($dishesRestaurant as $dish)
+                    <div class="row clearfix">
+                    <div class="col-xs-6 column" display:inline>
+                        <img src={{asset('uploads/'.($dish->id).'.jpg')}} alt="图片加载失败" width="100" height="100" class="thumbnail" >
+                    </div>
+                    <div class="col-xs-6 column" display:inline>
+                        <div >{{ $dish->name }}</div >
+                        <div >售价：{{ $dish->price }}</div >
+                        <div >{{ $dish->description }}</div >
+                         <p>
+                            <button onclick="ajaxLoad('/guest/create/{{$dish->id}}/{{$order ->id}}')"  class="btn"><i></i>添加</button>
+                         </p>
+                    </div>
+                    </div>
+                 <legend></legend>
+            @endforeach
             </div>
             @endif
+
             <!-- Current Dish -->
                 <a name="order"></a>
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Current Dishes for {{$restaurant->name}} in {{$table->alias}}
+                        已点菜品
                     </div>
 
                     <div class="panel-body">
                         <table class="table table-striped task-table">
                             <thead>
-                                <th>Dish</th>
-                                <th>price</th>
-                                <th>amount</th>
+                                <th>菜名</th>
+                                <th>单价</th>
+                                <th>数量</th>
                                 @if ($order->status !== "NotConfirm")
                                 <th>status</th>
                                 @endif
@@ -83,7 +76,7 @@
                                         @if ($ordersDishes[$i]->status == "NotStart")
                                         <td>
                                             <button onclick="dishDel('/guest/destroy/orderDish/{{$ordersDishes[$i]->id}}')" class="btn">
-                                                <i class="fa fa-btn fa-trash"></i>Delete
+                                                <i class="fa fa-btn fa-trash"></i>删除
                                             </button>
                                         </td>
                                         @endif
@@ -94,14 +87,14 @@
                         </table>
 
                         <div class="form-group">
-                        <label id="total" class="col-sm-3 control-label">Total: {{ $order->total}}</label>
+                        <label id="total" class="col-sm-3 control-label">总价: {{ $order->total}}</label>
                         </div>
                         @if (($order->status) == "NotConfirm")
                         <form action="/guest/confirm/{{$order ->id}}" method="POST">
                             {{ csrf_field() }}
                             <!-- {{ method_field('DELETE') }} -->
                             <div class="form-group">
-                            <label for="dish-name" class="col-sm-2 control-label">Remark</label>
+                            <label for="dish-name" class="col-sm-2 control-label">备注</label>
                             <div class="col-sm-10">
                                 <input type="text" name="remark" id="remark" class="form-control" placeholder="填写备注">
                             </div>
@@ -117,8 +110,6 @@
                         @endif
 
                     </div>
-                    </div>
                 </div>
-        </div>
-    </div>
+            </div>
 @endsection
